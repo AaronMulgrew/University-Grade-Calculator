2637aef38d425f94392b187fb38622611c3e14f8
Aaron <azz@live.ie>
2017-12-21 14:12:44 +0000

    added new test coverage for flask testing

--- unit_tests.py
+++ unit_tests.py
@@ -1,15 +1,41 @@
 import unittest
-import Uni_Grades_Calculator
+from app import app
+import app as application
 
 class Test_CalcOverall(unittest.TestCase):
+
+    def setUp(self):
+        # creates a test client
+        self.app = app.test_client()
+        # propagate the exceptions to the test client
+        self.app.testing = True 
+
     def test_CalcFunc(self):
-        self.assertFalse(Uni_Grades_Calculator.CalcOverall(0,0))
+        self.assertFalse(application.CalcOverall(0,0))
 
     def test_CalcFunc_returns_50(self):
-        self.assertEqual(Uni_Grades_Calculator.CalcOverall([100], [50]), '50.0')
+        self.assertEqual(application.CalcOverall([100], [50]), '50.0')
 
     def test_CalcFunc_returns_75(self):
-        self.assertEqual(Uni_Grades_Calculator.CalcOverall([50, 50], [50, 100]), '75.0')
+        self.assertEqual(application.CalcOverall([50, 50], [50, 100]), '75.0')
+
+
+class FlaskEndpointsTest(unittest.TestCase): 
+
+    def setUp(self):
+        # creates a test client
+        self.app = app.test_client()
+        # propagate the exceptions to the test client
+        self.app.testing = True 
+
+    def test_home_status_code(self):
+        # sends HTTP GET request to the application
+        # on the specified path
+        result = self.app.get('/') 
+
+        # assert the status code of the response
+        self.assertEqual(result.status_code, 200) 
+
 
 if __name__ == '__main__':
     unittest.main()
